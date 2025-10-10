a659b3976b33d99bf32c55c51e0b60a797870198
Mario Rodriguez <me@mario.life>
2025-10-10 05:04:07 -0700

    chore(small-updates): small updates to config files

--- my_flow_client/next.config.ts
+++ my_flow_client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: "/Users/mario/Projects/AZNext_AI_Builder_Projects/my_flow_app", // Absolute path to monorepo root
+  },
 };
 
 export default nextConfig;
